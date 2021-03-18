4e268d47ae373ccb7f78b69152235496d058a458
John Louis Rumingan <55402017+JohnLouisRumingan@users.noreply.github.com>
2021-03-18 15:43:26 -0400

    Change user component to allow name event handler

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-user',
+  templateUrl: 'user.component.html'
 })
 
 
-export class UserComponent{}
+export class UserComponent{
+  name = 'John-Louis'; //because string, TS implicitly attaches name: string to this property
+
+  onUserInput(event: any){
+    this.name = event.target.value;
+  }
+}

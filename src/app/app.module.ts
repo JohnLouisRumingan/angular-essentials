c5ad7e2446802f36ee12d8aa16c25a3b7348eaa7
John Louis Rumingan <55402017+JohnLouisRumingan@users.noreply.github.com>
2021-03-18 17:23:03 -0400

    Add LoadingComponent with different types of content binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user.component';
+import { LoadingComponent } from './loading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    LoadingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
